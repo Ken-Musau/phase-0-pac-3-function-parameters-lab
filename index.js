fa0192497030314dcdcd13f653a43d712e64430a
Kennedy Musau <kennedy.musauu@gmail.com>
2023-03-18 09:34:32 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,17 @@
+function introduction(name) {
+  return `Hi, my name is ${name}.`;
+}
+
+console.log(introduction("Kennedy"));
+
+function introductionWithLanguage(name, language) {
+  return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+}
+
+console.log(introductionWithLanguage("Mutunga", "JavaScript"));
+
+function introductionWithLanguageOptional(name, language = "JavaScript") {
+  return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+}
+
+console.log(introductionWithLanguageOptional("Musau"));
